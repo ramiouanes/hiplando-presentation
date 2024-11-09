1152839b0734bfcbfe45d21f52118ec5342b08eb
Rami Ouanes <rami.ouanes@panzura.com>
2024-11-09 12:22:37 +0100

    Fix vite config syntax

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/hiplando-presentation/',
   resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-build: {
+    extensions: ['.js', '.jsx']
+  },
+  build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 })
