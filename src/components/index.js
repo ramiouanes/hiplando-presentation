fd45578f470e1734582b25c13b6e03cb42630361
Rami Ouanes <rami.ouanes@panzura.com>
2024-11-09 12:38:20 +0100

    Fix import extensions

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as PresentationSection } from './PresentationSection.jsx';
-export { default as Pillar } from './Pillar.jsx';
-export { default as ProcessStep } from './ProcessStep.jsx';
-export { default as PricingTier } from './PricingTier.jsx';
+export { default as Pillar } from 'src/components/Pillar.jsx';
+export { default as ProcessStep } from 'src/components/ProcessStep.jsx';
+export { default as PricingTier } from 'src/components/PricingTier.jsx';
