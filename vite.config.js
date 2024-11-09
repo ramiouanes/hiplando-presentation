87a7fc91d5e3befc40803a521e1d0913f373f57b
Rami Ouanes <rami.ouanes@panzura.com>
2024-11-09 12:03:21 +0100

    Fix file paths and extensions

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hiplando-presentation/', // Matches your repository name
+  base: '/hiplando-presentation/',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 })
