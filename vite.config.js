4598312c50bffeed965dced38ce8d44b9bdf5e21
Rami Ouanes <rami.ouanes@panzura.com>
2024-11-09 12:19:06 +0100

    Fix vite permission issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/hiplando-presentation/',
-  build: {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+build: {
     outDir: 'dist',
     sourcemap: true
   }
