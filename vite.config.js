0ab7493bb65df4f4890d1c75a6eadce90c19aebd
Rami Ouanes <rami.ouanes@panzura.com>
2024-11-09 12:53:27 +0100

    Fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hiplando-presentation/'
+  base: '/hiplando-presentation/',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  }
 })
